6ed00886b3677c2da3a1c2b2f8639ed8ea8c60df
Arthur Miranda Gomes <arthur-m-gomes@hotmail.com>
2018-04-03 19:00:53 -0300

    update

--- Codes/AGReal/src/agreal/Individuo.java
+++ Codes/AGReal/src/agreal/Individuo.java
@@ -4,44 +4,29 @@ import java.util.ArrayList;
 import java.util.Random;
 
 /**
- *
  * @author fernando
  */
-public class Individuo implements Comparable<Individuo>{
-    
-    // Genotipo
-    private ArrayList<Integer> cromossomos;
+public class Individuo implements Comparable<Individuo> {
+
     // Fenotipo
     private ArrayList<Double> variaveis;
-    // Decodificado - binário para inteiro
-    private ArrayList<Integer> decodificacao;
+
     // Custo da função objetivo
     Double funcaoObjetivo;
-    
-    // Precisão
-    Integer precisao;
+
     // Valor mínimo
     Double minimo;
     // Valor máximo
     Double maximo;
-    
+
     // Número de variáveis
     Integer nVar;
 
-    public Individuo(Integer precisao, Double minimo, Double maximo, Integer nVar) {
-        this.precisao = precisao;
+    public Individuo(Double minimo, Double maximo, Integer nVar) {
         this.minimo = minimo;
         this.maximo = maximo;
         this.nVar = nVar;
-        this.cromossomos = new ArrayList<>();
-    }
-
-    public ArrayList<Integer> getCromossomos() {
-        return cromossomos;
-    }
-
-    public void setCromossomos(ArrayList<Integer> cromossomos) {
-        this.cromossomos = cromossomos;
+        this.variaveis = new ArrayList<>();
     }
 
     public ArrayList<Double> getVariaveis() {
@@ -52,28 +37,12 @@ public class Individuo implements Comparable<Individuo>{
         this.variaveis = variaveis;
     }
 
-    public ArrayList<Integer> getDecodificacao() {
-        return decodificacao;
-    }
-
-    public void setDecodificacao(ArrayList<Integer> decodificacao) {
-        this.decodificacao = decodificacao;
-    }
-
     public Double getFuncaoObjetivo() {
         return funcaoObjetivo;
     }
 
     public void setFuncaoObjetivo(Double funcaoObjetivo) {
         this.funcaoObjetivo = funcaoObjetivo;
-    }
-
-    public Integer getPrecisao() {
-        return precisao;
-    }
-
-    public void setPrecisao(Integer precisao) {
-        this.precisao = precisao;
     }
 
     public Double getMinimo() {
@@ -99,55 +68,21 @@ public class Individuo implements Comparable<Individuo>{
     public void setnVar(Integer nVar) {
         this.nVar = nVar;
     }
-    
+
     // Gerar o genótipo
     public void criar() {
-        
-        this.cromossomos = new ArrayList<>();
-        
-        Random rnd = new Random();
-        
-        for(int i = 0; i < this.getnVar(); i++) {
-            for(int j = 0; j < this.getPrecisao(); j++) {
-                this.getCromossomos().add( rnd.nextInt(2) );
-            }
-        }
-        
-    }
-    
-    // Genótipo -> Fenótipo : Binário -> Real
-    public void decodificar() {
-        
-        this.decodificacao = new ArrayList<>();
+
         this.variaveis = new ArrayList<>();
 
+        Random rnd = new Random();
+        Double valor;
 
-       int valor;
-       Double real;
-       
-       for(int i = 0; i < this.getnVar(); i++) {
-       
-           valor = 0;
-           
-           for(int j = 0; j < this.getPrecisao(); j++) {
-               
-               valor += Math.pow(2, this.getPrecisao() - j - 1)
-                       * this.getCromossomos().get( i * this.getPrecisao() + j );
-               
-           }
-           
-           this.getDecodificacao().add(valor);
-           
-           real = (valor * ( this.getMaximo() - this.getMinimo() )
-                    / ( Math.pow(2, this.getPrecisao()) - 1.0 )) 
-                   + this.getMinimo();
-           
-           this.getVariaveis().add(real);
-           
-       }
-
-        
+        for (int i = 0; i < this.getnVar(); i++) {
+            valor = this.minimo + (this.maximo - this.minimo) * rnd.nextDouble();
+            this.variaveis.add(valor);
+        }
     }
+
 
     @Override
     public int compareTo(Individuo o) {
