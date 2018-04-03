2b6b4f5c7c65b8fac708d786466accd2eec222cb
Arthur Miranda Gomes <arthur-m-gomes@hotmail.com>
2018-04-03 18:45:02 -0300

    Update

---
+++ Codes/AGReal/src/agreal/Problema.java
@@ -0,0 +1,23 @@
+package agreal;
+
+/**
+ *
+ * @author fernando
+ */
+public class Problema {
+    
+    public void calcularFuncaoObjetivo(Individuo individuo) {
+        
+        Double soma = 0.0;
+        
+        individuo.decodificar();
+        
+        for( Double var : individuo.getVariaveis() ) {
+            soma += Math.pow(var, 2);
+        }
+        
+        individuo.setFuncaoObjetivo(soma);
+        
+    }
+    
+}
