1cf67ec3dad29a5791e3ec2cc6728a0717d44486
Arthur Miranda Gomes <arthur-m-gomes@hotmail.com>
2018-04-05 21:39:18 -0300

    update

--- Analysis/analise_ag_bin_real.R
+++ Analysis/analise_ag_bin_real.R
@@ -1,8 +1,15 @@
 rm(list = rm())
 
 dados  <- read.csv2("data/data-ag-bin-real.txt")
+dados$FO <- as.numeric(as.character.numeric_version(dados$FO))
+
 
 library(plyr)
 variavel  <- count(dados,c("Teste"))
 
-#min(dados$FO)
+show(min(dados$FO))
+show(max(dados$FO))
+show(sd(dados$F0))
+
+
+boxplot(FO~Teste, data = dados)
